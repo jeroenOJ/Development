8248e1543a83343cd64e2422986e14fd31e3e23b
Jeroen Hartman <jeroen@itlor.local>
2016-04-13 12:40:31 +0200

    test

---
+++ Testpush/Testpush/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Testpush.MvcApplication" Language="C#" %>
---
+++ Testpush/Testpush/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testpush.Startup))]
+namespace Testpush
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
